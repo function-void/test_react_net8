7c0dc04c8f7038781a286f632376e1b6761d0657
Amirbek Akhmetov <lieberkz99@gmail.com>
2025-06-18 22:13:25 +0500

    [add] option pattern and add system user

---
+++ backend/Options/ConfigurationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Options;
+using UserManagementApp.Infrastructure.Services.Options;
+
+namespace UserManagementApp.Options;
+
+public static class ConfigurationExtensions
+{
+    public static IServiceCollection AddConfigureOptions(this IServiceCollection services)
+    {
+        services.ConfigureOptions<SystemUserSetup>();
+
+        services.AddSingleton(x => x.GetService<IOptions<SystemUserOptions>>()!.Value);
+
+        return services;
+    }
+}
---
+++ backend/Options/SystemUserSetup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Options;
+using UserManagementApp.Infrastructure.Services.Options;
+
+namespace UserManagementApp.Options;
+
+public class SystemUserSetup(IConfiguration configuration) : IConfigureOptions<SystemUserOptions>
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    public void Configure(SystemUserOptions options)
+    {
+        _configuration.GetSection(SystemUserOptions.SectionName).Bind(options);
+    }
+}
