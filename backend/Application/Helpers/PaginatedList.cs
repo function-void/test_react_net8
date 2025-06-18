0c456e5c3fb79416aa54a714596739f895e27c50
Amirbek Akhmetov <lieberkz99@gmail.com>
2025-06-18 23:05:22 +0500

    [add] pagination model

---
+++ backend/Application/Helpers/PaginatedList.cs
@@ -0,0 +1,17 @@
+﻿namespace UserManagementApp.Application.Helpers;
+
+public class PaginatedList<T>
+{
+    public IEnumerable<T> Items { get; set; } = [];
+    public int TotalCount { get; set; }
+    public int TotalPages { get; set; }
+    public int PageNumber { get; set; }
+
+    public PaginatedList(List<T> items, int count, int pageNumber, int pageSize)
+    {
+        PageNumber = pageNumber;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        TotalCount = count;
+        Items = items;
+    }
+}
---
+++ backend/Application/Helpers/PaginatedQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace UserManagementApp.Application.Helpers;
+
+public class PaginatedQuery
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+}
