c90ffed731c618d56b82561e4af903898837a9de
Amirbek Akhmetov <lieberkz99@gmail.com>
2025-06-18 22:14:19 +0500

    [add] user

---
+++ backend/Domain/Models/User.cs
@@ -0,0 +1,9 @@
+namespace UserManagementApp.Domain.Models;
+
+public class User : SoftAuditableEntity
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Role { get; set; } = null!;
+}
