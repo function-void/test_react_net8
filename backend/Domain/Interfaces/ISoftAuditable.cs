5d30cff29fd88b781c7c65f5b0e9d1377a611871
Amirbek Akhmetov <lieberkz99@gmail.com>
2025-06-18 22:14:06 +0500

    [add] soft auditable entity

---
+++ backend/Domain/Interfaces/ISoftAuditable.cs
@@ -0,0 +1,12 @@
+﻿namespace UserManagementApp.Domain.Interfaces;
+
+public interface ISoftAuditable
+{
+    DateTime CreatedAt { get; set; }
+    DateTime? LastModifiedAt { get; set; }
+    DateTime? DeletedAt { get; set; }
+    string CreatedBy { get; set; }
+    string? LastModifiedBy { get; set; }
+    string? DeletedBy { get; set; }
+    bool IsDeleted { get; set; }
+}
---
+++ backend/Domain/Models/SoftAuditable.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using UserManagementApp.Domain.Interfaces;
+
+namespace UserManagementApp.Domain.Models;
+
+public abstract class SoftAuditableEntity : ISoftAuditable
+{
+    public DateTime CreatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public DateTime? LastModifiedAt { get; set; }
+    public string CreatedBy { get; set; } = null!;
+    public string? LastModifiedBy { get; set; }
+    public string? DeletedBy { get; set; }
+    public bool IsDeleted { get; set; }
+}
